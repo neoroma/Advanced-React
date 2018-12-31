edf3706047df88619726bc2974ddf9567ba8c479
Roman Shestakov <neoroma@gmail.com>
2018-12-31 12:51:58 +0200

    Add nprogress progerss bar

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -2,6 +2,21 @@ import React from 'react'
 import Link from 'next/link'
 import styled, { ThemeProvider } from 'styled-components'
 
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+}
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+}
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+}
+
 const Logo = styled.h2`
   font-size: 4rem;
   margin-left: 2rem;
