fa304e1c18197fb430b731660b120e9ec5c09f87
Roman Shestakov <neoroma@gmail.com>
2018-12-31 11:48:02 +0200

    Add page and header styles

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,13 +1,43 @@
 import React from 'react'
+import styled, { ThemeProvider, injectGlobal } from 'styled-components'
+import Header from './Header'
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+}
+
+const StyledPage = styled.div`
+  background: whitesmoke;
+  color: ${props => props.theme.black};
+`
+
+StyledPage.displayName = 'PageMain'
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  background: wheat;
+  padding: 2rem;
+`
+
+Inner.displayName = "InnerThingy"
 
 export default class Page extends React.Component {
   render() {
     return (
-      <div>
-        <h3>Hey this is the page component, we can add header and nav here</h3>
-        {this.props.children}
-        <h3>It can be the footer here haha</h3>
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Header/>
+          <Inner>{this.props.children}</Inner>
+          <h3>It can be the footer here haha</h3>
+        </StyledPage>
+      </ThemeProvider>
     )
   }
 }
